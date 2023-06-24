a343f1fa3c343d99d2288969bf9c2a5d5174143b
Rashan Smith <smith.rashan@gmail.com>
2023-06-23 20:00:47 -0400

    Add code to connect with flask backend

---
+++ src/app/components/Pandas_AI.js
@@ -0,0 +1,90 @@
+'use client'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+
+export default function YourComponent({ data }) {
+    const [dataFrame, setDataFrame] = useState(null);
+    let [loading, setLoading] = useState(true);
+    // const [data, setData] = useState([]);
+    const [htmlTable, setHtmlTable] = useState('');
+    
+    // useEffect(() => {
+    //     const fetchProjectData = async () => {
+
+    //         if (sendRequest && prompt) {
+    //             try {
+    //                 const response = await axios.post('http://localhost:5000/', {
+    //                     table_name: tableName,
+    //                     prompt: ` ${prompt} `
+    //                 });
+
+    //                 console.log(typeof response.data)
+    //                 console.log(response)
+    //                 setData(data)
+            
+
+    //             } catch (error) {
+    //                 console.error('Error:', error);
+    //             }
+    //         }
+    //     };
+
+    //     fetchProjectData();
+    // }, [sendRequest]);
+
+    const formatValue = (value) => {
+        return JSON.stringify(value)
+    };
+
+    const getImage = (value) => {
+        //console.log(JSON.stringify(value.image))
+        const new_value = value.image
+        const imageUrl = `data:image/png,base64,${new_value}`;
+        console.log(imageUrl)
+        return imageUrl
+    }
+
+
+
+    // Render the data
+    return (
+        <div>
+        {typeof data === 'number' || typeof data === 'string' ? (
+              <div>{data}</div>
+        ) : (
+            <div>
+                {typeof data === 'object' && !data.image  ? (
+                    Object.keys(data).map(key => (
+                    <div key={key}>
+                        <p className="pt-4 pb-2">
+                        <strong>{key}:<br/></strong>
+                        </p>
+                        {Object.entries(data[key]).map(([k, v]) => (
+                        <p key={k} className="pt-2 pb-2">
+                            {k}: {v}
+                        </p>
+                        ))}
+                    </div>
+                    )) 
+                    ) : (
+                        <div>
+                            {data.image ? (
+                                <div>
+                                    <img
+                                        src={`data:image/jpeg;base64,${data.image}`} 
+                                        alt="Base64 Image"   
+                                        width={800}
+                                        height={1000}/>
+                                </div>
+                            ) :
+                                <p>Loading results...</p> 
+                             }
+                        </div>
+                )}
+          </div>
+
+        )}
+        </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,46 @@
+'use client'
 import Link from 'next/link';
+import Prompt_Results from './components/Pandas_AI';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+  const [prompt, setPrompt] = useState('')
+  const [tableName, setTableName ] = useState('Projects')
+  const [sendRequest, setSendRequest ] = useState(false)
+  const [data, setData ] = useState('')
+
+  const handlePromptChange = (event) => {
+    // console.log(prompt)
+    setPrompt(event.target.value);
+  };
+
+
+  const submitRequest = () => {
+    setSendRequest(true);
+    fetchProjectData()
+  };
+
+  const fetchProjectData = async () => {
+    console.log(prompt)
+    if (prompt && tableName) {
+      try {
+        const response = await axios.post('http://localhost:5000/', {
+          table_name: tableName,
+          prompt: ` ${prompt} `
+        });
+
+        console.log(typeof response.data);
+        console.log(response);
+        setData(response.data);
+
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+  };
+
+
   return (
     <div className="flex flex-col items-center justify-center bg-gray-100">
       {/* Header */}
@@ -36,29 +76,26 @@ export default function Home() {
           <div className="w-full min-h-screen p-8">
             {/* Content for right half */}
             <div className="flex flex-col items-center justify-start max-h-screen">
-              <select className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 mb-4">
-                <option value="option0" disabled selected>
-                  Select an Excel Data Table
-                </option>
-                <option value="option1">Summary</option>
-                <option value="option2">Countries</option>
-                <option value="option3">Outputs</option>
+              {/* <select className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 mb-4">
                 <option value="option3">Projects</option>
-              </select>
+                <option value="option3">Outputs</option>
+              </select> */}
               <div className="w-full flex items-center justify-center relative mb-4">
                 <textarea
+                  value={prompt} 
+                  onChange={handlePromptChange} 
                   className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 resize-none"
                   rows={4}
                   placeholder="Enter prompt here.."
                 />
-                <button className="absolute bottom-2 right-2 bg-gray-500 hover:bg-gray-600 text-white text-sm font-semibold px-4 py-2 rounded-md">
+                <button onClick={submitRequest} className="absolute bottom-2 right-2 bg-gray-500 hover:bg-gray-600 text-white text-sm font-semibold px-4 py-2 rounded-md">
                   Submit
                 </button>
               </div>
               {/* Results */}
               <div className="w-full max-h-screen bg-gray-200">
                 <div className="p-8">
-                  <p>Results go here</p>
+                  <Prompt_Results sendRequest={sendRequest} prompt={prompt} tableName={tableName} data={data} />
                 </div>
               </div>
             </div>
